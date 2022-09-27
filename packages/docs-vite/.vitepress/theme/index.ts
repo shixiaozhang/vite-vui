1becf80482d7355da21dc862a5ce0e044fffc315
xiaozhang <813155893@qq.com>
2022-09-27 14:38:56 +0800

    refactor: 基于pnpm的workspace的monorepo

--- packages/docs-vite/.vitepress/theme/index.ts
+++ packages/docs-vite/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
-import SmartyUI from '../../../src/entry'
+import SmartyUI from '../../../vite-ui/src/entry'
 // 插件的组件，主要是demo组件
 export default {
   ...DefaultTheme,
